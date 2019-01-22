061f3d9d88a5af7e9453c0dc81cfa227b6c657a3
Maythiwat Chomchuen <maythiwat.chom@gmail.com>
2019-01-22 21:24:13 +0700

    FetchActivities default params value

--- WalletAPI.php
+++ WalletAPI.php
@@ -9,66 +9,68 @@ class WalletAPI {
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-        if ($header) curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        if ($data) curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, (!$header) ? false : $header);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, (!$data) ? false : $data);
         return curl_exec($ch);
     }
 
     public function GetToken($user, $pass, $type = 'email') {
         $url = "https://mobile-api-gateway.truemoney.com/mobile-api-gateway/api/v1/signin";
-        $header = array("Host: mobile-api-gateway.truemoney.com", "Content-Type: application/json");
-        $data = array("username"=>$user, "password"=>sha1($user.$pass), "type"=>$type);
+        $header = ["Host: mobile-api-gateway.truemoney.com", "Content-Type: application/json"];
+        $data = ["username"=>$user, "password"=>sha1($user.$pass), "type"=>$type];
         return @json_decode($this->Request('POST', $url, $header, json_encode($data)), true)['data']['accessToken'];
     }
     
     public function Logout($token) {
         $url = "https://mobile-api-gateway.truemoney.com/mobile-api-gateway/api/v1/signout/{$token}";
-        $header = array("Host: mobile-api-gateway.truemoney.com");
+        $header = ["Host: mobile-api-gateway.truemoney.com"];
         return @json_decode($this->Request('GET', $url, $header, false), true);
     }
     
     public function GetCurrentBalance($token) {
         $url = "https://mobile-api-gateway.truemoney.com/mobile-api-gateway/api/v1/profile/balance/{$token}";
-        $header = array("Host: mobile-api-gateway.truemoney.com");
+        $header = ["Host: mobile-api-gateway.truemoney.com"];
         return @json_decode($this->Request('GET', $url, $header, false), true)['data'];
     }
 
     public function GetProfile($token) {
         $url = "https://mobile-api-gateway.truemoney.com/mobile-api-gateway/api/v1/profile/{$token}";
-        $header = array("Host: mobile-api-gateway.truemoney.com");
+        $header = ["Host: mobile-api-gateway.truemoney.com"];
         return @json_decode($this->Request('GET', $url, $header, false), true)['data'];
     }
 
-    public function FetchActivities($token, $start, $end, $limit = 25) {
-        $url = "https://mobile-api-gateway.truemoney.com/mobile-api-gateway/user-profile-composite/v1/users/transactions/history?start_date={$start}&end_date={$end}&limit={$limit}";
-        $header = array("Host: mobile-api-gateway.truemoney.com", "Authorization: {$token}");
+    public function FetchActivities($token, $start = null, $end = null, $limit = 25) {
+        $end = ($end == null) ? date('Y-m-d') : $end;
+        $start = ($start == null) ? date('Y-m-d', strtotime('-7 days')) : $start;
+        $url = "https://mobile-api-gateway.truemoney.com/mobile-api-gateway/api/v1/profile/transactions/history/{$token}/?startDate={$start}&endDate={$end}&limit={$limit}";
+        $header = ["Host: mobile-api-gateway.truemoney.com"];
         return @json_decode($this->Request('GET', $url, $header, false), true)['data']['activities'];
     }
 
     public function FetchTxDetail($token, $id) {
-        $url = "https://mobile-api-gateway.truemoney.com/mobile-api-gateway/user-profile-composite/v1/users/transactions/history/detail/{$id}";
-        $header = array("Host: mobile-api-gateway.truemoney.com", "Authorization: {$token}");
+        $url = "https://mobile-api-gateway.truemoney.com/mobile-api-gateway/api/v1/profile/activities/{$id}/detail/{$token}";
+        $header = ["Host: mobile-api-gateway.truemoney.com"];
         return @json_decode($this->Request('GET', $url, $header, false), true)['data'];
     }
 
     public function CashcardTopup($token, $cashcard) {
         $time = time();
         $url = "https://mobile-api-gateway.truemoney.com/mobile-api-gateway/api/v1/topup/mobile/{$time}/{$token}/cashcard/{$cashcard}";
-        $header = array("Host: mobile-api-gateway.truemoney.com");
+        $header = ["Host: mobile-api-gateway.truemoney.com"];
         return @json_decode($this->Request('POST', $url, $header, true), true);
     }
     
     public function CashcardBuyRequest($token, $mobile, $amount) {
         $url = "https://mobile-api-gateway.truemoney.com/mobile-api-gateway/api/v1/buy/e-pin/draft/verifyAndCreate/{$token}";
-        $data = array("recipientMobileNumber"=>$mobile,"amount"=>$amount);
-        $header = array("Host: mobile-api-gateway.truemoney.com", "Content-Type: application/json");
+        $data = ["recipientMobileNumber"=>$mobile, "amount"=>$amount];
+        $header = ["Host: mobile-api-gateway.truemoney.com", "Content-Type: application/json"];
         return @json_decode($this->Request('POST', $url, $header, $data), true);
     }
     
     public function CashcardBuyComfirm($token, $draft, $mobile, $otpString, $otpRefCode) {
         $url = "https://mobile-api-gateway.truemoney.com/mobile-api-gateway/api/v1/buy/e-pin/confirm/{$draft}/{$token}";
-        $data = array("mobileNumber"=>$mobile, "otpString"=>$otpString, "otpRefCode"=>$otpRefCode, "timestamp"=>time());
-        $header = array("Host: mobile-api-gateway.truemoney.com", "Content-Type: application/json");
+        $data = ["mobileNumber"=>$mobile, "otpString"=>$otpString, "otpRefCode"=>$otpRefCode, "timestamp"=>time()];
+        $header = ["Host: mobile-api-gateway.truemoney.com", "Content-Type: application/json"];
         return @json_decode($this->Request('PUT', $url, $header, $data), true);
     }
 }
